0952fc75e705ca04184edccef657cda105c0fdbe
Krantz-XRF <Krantz.XRF@outlook.com>
2019-05-24 17:01:34 +0800

    Replace the colons with ASCII colons
    Before this commit the colons were left out as Chinese ones

--- lava/config/en.h
+++ lava/config/en.h
@@ -1,7 +1,7 @@
 #pragma once
-#define msg_should_not_move_to_this "Should not copy a resource object to itself."
+#define msg_should_not_move_to_this "Should not move a resource object to itself."
 #define msg_error_msg(file, line, func, msg) "error: \"", file, "\":", line, ':', func, ": ", msg
 #define msg_panic "panic: "
-#define msg_unreachable_code_reached "unreachable codes are reached£º"
+#define msg_unreachable_code_reached "unreachable codes are reached: "
 #define msg_assert_type_names "pre-condition", "post-condition", "invariant"
-#define msg_condition_not_satisfied " is not satisfied. message£º"
+#define msg_condition_not_satisfied " is not satisfied. message: "
