ac58e090eda017b1c61255bf15a1dac77edeb745
Krantz-XRF <Krantz.XRF@outlook.com>
2019-11-06 23:23:45 +0800

    Restructure: lava.curry
    - introduce namespace detail
    - hide implementation details in lava::curry::detail

--- lava/curry.h
+++ lava/curry.h
@@ -5,75 +5,92 @@
 
 namespace lava::curry
 {
-	template<typename Res, typename... Args>
-	struct curry_impl;
+	template<typename F>
+	decltype(auto) curry(F&& f);
 
-	template<typename Res, typename... Args>
-	using curry_t = typename curry_impl<Res(Args...)>::type;
-
-	template<typename Res, typename... Args>
-	curry_t<Res, Args...> curry(std::function<Res(Args...)> f)
+	namespace detail
 	{
-		return curry_impl<Res(Args...)>::curry(f);
-	}
+		template<typename Res, typename... Args>
+		struct curry_impl;
 
-	template<typename Res, typename... Args>
-	decltype(auto) curry(Res (*f)(Args...))
-	{
-		return curry(std::function<Res(Args...)>(f));
-	}
+		// Type of the curried function
+		template<typename Res, typename... Args>
+		using curry_t = typename curry_impl<Res(Args...)>::type;
 
-	template <typename T>
-	struct lambda_to_function;
+		// Get function signature of various function-like objects
+		template<typename T>
+		struct signature;
 
-	template <typename C,typename Res, typename ...Args>
-	struct lambda_to_function<Res(C::*)(Args...)>
-	{
-		using type = Res(Args...);
-	};
+		// Signature for plain old function pointers
+		template<typename Res, typename... Args>
+		struct signature<Res (*)(Args...)>
+		{
+			using type = Res(Args...);
+		};
 
-	template <typename C,typename Res, typename ...Args>
-	struct lambda_to_function<Res(C::*)(Args...) const>
-	{
-		using type = Res(Args...);
-	};
+		// Signature for std::function
+		template<typename Res, typename... Args>
+		struct signature<std::function<Res(Args...)>>
+		{
+			using type = Res(Args...);
+		};
 
-	template <typename T>
-	using lambda_to_function_t = typename lambda_to_function<T>::type;
+		// Signature for member functions
+		template<typename C, typename Res, typename... Args>
+		struct signature<Res (C::*)(Args...)>
+		{
+			using type = Res(Args...);
+		};
 
+		// Signature for const member functions
+		// (with constness removed)
+		template<typename C, typename Res, typename... Args>
+		struct signature<Res (C::*)(Args...) const>
+		{
+			using type = Res(Args...);
+		};
+
+		// Base case: no parameter
+		template<typename Res>
+		struct curry_impl<Res()>
+		{
+			using type = std::function<Res()>;
+			static type curry(type f) { return f; }
+		};
+
+		// Base case: 1 parameter
+		template<typename Res, typename Arg>
+		struct curry_impl<Res(Arg)>
+		{
+			using type = std::function<Res(Arg)>;
+			static type curry(type f) { return f; }
+		};
+
+		// Recursive case: 2 or more parameters
+		template<typename Res, typename Arg, typename... Args>
+		struct curry_impl<Res(Arg, Args...)>
+		{
+			using type = std::function<curry_t<Res, Args...>(Arg)>;
+			static type curry(std::function<Res(Arg, Args...)> f)
+			{
+				return [f = std::move(f)](Arg x) -> curry_t<Res, Args...>
+				{
+					return lava::curry::curry(std::function<Res(Args...)>(
+						[x = std::move(x), f = std::move(f)](Args... args) {
+							return f(x, args...);
+						}));
+				};
+			}
+		};
+	} // namespace detail
+
+	template<typename T>
+	using signature_t = typename detail::signature<T>::type;
+
+	// the curry function
 	template<typename F>
 	decltype(auto) curry(F&& f)
 	{
-		return curry_impl<lambda_to_function_t<decltype(&F::operator())>>::curry(f);
+		return detail::curry_impl<signature_t<decltype(&F::operator())>>::curry(f);
 	}
-
-	template<typename Res>
-	struct curry_impl<Res()>
-	{
-		using type = std::function<Res()>;
-		static type curry(type f) { return f; }
-	};
-
-	template<typename Res, typename Arg>
-	struct curry_impl<Res(Arg)>
-	{
-		using type = std::function<Res(Arg)>;
-		static type curry(type f) { return f; }
-	};
-
-	template<typename Res, typename Arg, typename... Args>
-	struct curry_impl<Res(Arg, Args...)>
-	{
-		using type = std::function<curry_t<Res, Args...>(Arg)>;
-		static type curry(std::function<Res(Arg, Args...)> f)
-		{
-			return [f = std::move(f)](Arg x) -> curry_t<Res, Args...>
-			{
-				return lava::curry::curry(std::function<Res(Args...)>(
-					[x = std::move(x), f = std::move(f)](Args... args) {
-						return f(x, args...);
-					}));
-			};
-		}
-	};
 } // namespace lava::curry
